2cc46354b00f08fdc23d562f37d79d02ea811529
Simon <kh931623@hotmail.com>
2021-12-12 03:40:49 +0800

    finished parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,36 @@
-const parse = () => ''
+const R = require('ramda')
+
+const parseA = (code) => {
+  return {
+    type: 'A',
+    address: code.slice(0)
+  }
+}
+
+const parseC = R.pipe(
+  R.match(/(\w*)=?([A-Z0-9-]*);?(\w*)/),
+  R.props([
+    1,
+    2,
+    3
+  ]),
+  R.zipObj([
+    'dest',
+    'comp',
+    'jump'
+  ]),
+  R.assoc('type', 'C')
+)
+
+const getParser = R.cond([
+  [R.startsWith('@'), R.always(parseA)],
+  [R.T, R.always(parseC)]
+])
+
+const parse = (code) => {
+  const parser = getParser(code)
+
+  return parser(code)
+}
 
 module.exports = parse
