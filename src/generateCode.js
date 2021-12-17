060afa25e45d6e7292b45148e48b9317b9723638
Simon <kh931623@hotmail.com>
2021-12-18 02:03:09 +0800

    finish generateCode

--- src/generateCode.js
+++ src/generateCode.js
@@ -11,7 +11,55 @@ const generateA = R.pipe(
   padStart(16, '0')
 )
 
-const generateComp = () => 'comp'
+const getAluInstructions = R.prop(R.__, {
+  0: '101010',
+  1: '111111',
+  '-1': '111010',
+  D: '001100',
+  A: '110000',
+  M: '110000',
+  '!D': '001101',
+  '!A': '110001',
+  '!M': '110001',
+  '-D': '001111',
+  '-A': '110011',
+  '-M': '110011',
+  'D+1': '011111',
+  'A+1': '110111',
+  'M+1': '110111',
+  'D-1': '001110',
+  'A-1': '110010',
+  'M-1': '110010',
+  'D+A': '000010',
+  'D+M': '000010',
+  'D-A': '010011',
+  'D-M': '010011',
+  'A-D': '000111',
+  'M-D': '000111',
+  'D&A': '000000',
+  'D&M': '000000',
+  'D|A': '010101',
+  'D|M': '010101'
+})
+
+const getCompA = R.ifElse(
+  R.includes('M'),
+  R.always('1'),
+  R.always('0')
+)
+
+const getComp = R.pipe(
+  R.juxt([
+    getCompA,
+    getAluInstructions
+  ]),
+  R.apply(R.concat)
+)
+
+const generateComp = R.pipe(
+  R.propOr('', 'comp'),
+  getComp
+)
 
 const getDest = R.prop(R.__, {
   M: '001',
