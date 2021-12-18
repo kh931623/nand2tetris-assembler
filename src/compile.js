92d1e66e786dfe0429f22fa54eb0c5ee38c2fa5b
Simon <kh931623@hotmail.com>
2021-12-18 17:51:40 +0800

    modify flow of compile to include symbol map part

--- src/compile.js
+++ src/compile.js
@@ -2,6 +2,9 @@ const R = require('ramda')
 
 const parse = require('./parse.js')
 const generateCode = require('./generateCode.js')
+const {
+  constructSymbolMap
+} = require('./symbols.js')
 
 const eitherCommentOrBlank = R.either(
   R.startsWith('//'),
@@ -10,25 +13,32 @@ const eitherCommentOrBlank = R.either(
 
 const getActualCodeLines = R.pipe(
   R.split('\n'),
-  R.reject(eitherCommentOrBlank)
+  R.reject(eitherCommentOrBlank),
+  R.map(R.trim)
 )
 
-const tranformCode = R.pipe(
-  R.trim,
-  parse,
-  generateCode
-)
+const tranformCode = R.curry((symbolMap, code) => {
+  return R.pipe(
+    parse,
+    generateCode(symbolMap)
+  )(code)
+})
 
 const compile = (content) => {
   // get actual codes
   const lines = getActualCodeLines(content)
 
   // first pass to construct symbol Map
+  const symbolMap = constructSymbolMap(lines)
 
   // actual compilation
-  const results = R.map(tranformCode, lines)
-
-  return results.join('\n')
+  return R.pipe(
+    R.reject(R.startsWith('(')),
+    R.map(
+      tranformCode(symbolMap)
+    ),
+    R.join('\n')
+  )(lines)
 }
 
 module.exports = compile
